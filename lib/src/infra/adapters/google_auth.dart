91546ab38dac74e40cd4d7f6351039fbb5d0f445
JoseFlorez <tiomix68@gmail.com>
2021-11-09 22:08:01 -0500

    Authentication
    Authentication

--- lib/src/infra/adapters/google_auth.dart
+++ lib/src/infra/adapters/google_auth.dart
@@ -1,5 +1,6 @@
 import 'package:async/src/result/result.dart';
 import 'package:food_ordering/src/domain/auth_service_contract.dart';
+import 'package:food_ordering/src/domain/credential.dart';
 import 'package:food_ordering/src/domain/token.dart';
 import 'package:food_ordering/src/infra/api/auth_api_contract.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -17,12 +18,32 @@ class GoogleAuth implements IAuthService {
 
   @override
   Future<Result<Token>> signIn() async {
-    //36:52 del video
+    await _handleGoogleSignIn();
+    if (_currentUser == null) {
+      return Result.error('Faild to signin with Google');
+    }
+    Credential credential = Credential(
+        type: AuthType.google,
+        name: _currentUser.displayName!,
+        email: _currentUser.email,
+        password: '');
+    var result = await _authApi.signIn(credential);
+    if (result.isError) {
+      return result.asError!;
+    }
+    return Result.value(Token(result.asValue!.value));
   }
 
   @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
+  Future<void> signOut() async {
+    _googleSignIn.disconnect();
+  }
+
+  _handleGoogleSignIn() async {
+    try {
+      _currentUser = (await _googleSignIn.signIn())!;
+    } catch (error) {
+      return;
+    }
   }
 }
